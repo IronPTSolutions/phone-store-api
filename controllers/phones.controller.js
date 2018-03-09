691031e32cc947e66060763c7a6bc126e9f6de8f
Carlos del Prado <carlos.delprado@11paths.com>
2018-03-09 23:47:34 +0100

    feat: update phone

--- controllers/phones.controller.js
+++ controllers/phones.controller.js
@@ -28,7 +28,7 @@ module.exports.create = (req, res, next) => {
   const phone = new Phone(req.body);
   phone.save()
     .then(() => {
-      res.status(200).json(phone);
+      res.status(201).json(phone);
     })
     .catch(error => {
       if (error instanceof mongoose.Error.ValidationError) {
@@ -57,5 +57,17 @@ module.exports.delete = (req, res, next) => {
 }
 
 module.exports.edit = (req, res, next) => {
-  res.json({ message: 'Unimplemented' });
+  const id = req.params.id;
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    Phone.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+      .then(phone => {
+        if (phone) {
+          res.status(200).json(phone)
+        } else {
+          next(new ApiError(`Phone not found`, 404));
+        }
+      }).catch(error => next(error));
+  } else {
+    next(new ApiError(`Invalid phone id: ${id}`));
+  }
 }
