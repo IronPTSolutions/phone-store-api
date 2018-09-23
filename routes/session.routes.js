15250df242b21484d1fde961ff49496c5fa313e4
Carlos del Prado <carlos.delprado@11paths.com>
2018-09-23 11:33:41 +0200

    feat: clean version

--- routes/session.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const secureMiddleware = require('../middleware/secure.middleware');
-const sessionController = require('../controllers/session.controller');
-
-router.post('/', sessionController.create);
-router.delete('/', sessionController.destroy);
-
-module.exports = router;
--- routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const usersController = require('../controllers/users.controller');
-
-router.post('/', usersController.create);
-
-module.exports = router;
